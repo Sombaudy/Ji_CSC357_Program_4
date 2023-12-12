936676a073d6ed02d1808f7acc681c6179e8f8d4
Sombaudy <jhji@unix3.csc.calpoly.edu>
2023-12-11 23:26:25 -0800

    files added

---
+++ httpd.c
@@ -0,0 +1,73 @@
+#define _GNU_SOURCE
+#include "net.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <string.h>
+
+#define PORT 2828
+
+void handle_request(int nfd)
+{
+   FILE *network = fdopen(nfd, "r");
+   char *line = NULL;
+   size_t size;
+   ssize_t num;
+
+   if (network == NULL)
+   {
+      perror("fdopen");
+      close(nfd);
+      return;
+   }
+
+   while ((num = getline(&line, &size, network)) >= 0)
+   {
+      printf("received from client: %s", line);
+
+      char copy[100];
+      strcpy(copy, line);
+      printf("copy: %s", copy);
+
+      if(send(nfd, copy, sizeof(copy), 0) != sizeof(copy)) {
+         perror("send error");
+         close(nfd);
+         return;
+      }
+   }
+
+   free(line);
+   fclose(network);
+}
+
+void run_service(int fd)
+{
+   while (1)
+   {
+      int nfd = accept_connection(fd);
+      if (nfd != -1)
+      {
+         printf("Connection established\n");
+         handle_request(nfd);
+         printf("Connection closed\n");
+      }
+   }
+}
+
+int main(void)
+{
+   int fd = create_service(PORT);
+
+   if (fd == -1)
+   {
+      perror(0);
+      exit(1);
+   }
+
+   printf("listening on port: %d\n", PORT);
+   run_service(fd);
+   close(fd);
+
+   return 0;
+}
